6aa76b153b43889946a32be922f9e89a94a8c0ac
Patricia Duran Castro <pcastro@gamma05.aulas.gsyc.urjc.es>
2016-09-19 10:35:27 +0200

    Añado multiplicar

--- calc.py
+++ calc.py
@@ -13,6 +13,16 @@ def minus(op1, op2):
     """ Function to substract the operands """
     return op1 - op2
 
+
+def mult(op1, op2):
+	return op1 * op2
+
+
+def div(op1, op2):
+	return op1 / op2
+
+
+
 if __name__ == "__main__":
     try:
         operando1 = int(sys.argv[1])
@@ -22,8 +32,15 @@ if __name__ == "__main__":
 
     if sys.argv[2] == "suma":
         result = plus(operando1, operando2)
+
     elif sys.argv[2] == "resta":
         result = minus(operando1, operando2)
+
+	elif sys.argv[2] == "multiplicar":
+		result = mult(operando1, operando2)
+
+	
+
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
 
