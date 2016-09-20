e77414d9d6bc77fa261d0701b149577f2d17a9fc
pdurancastro <patryharuno@gmail.com>
2016-09-20 22:04:58 +0200

    Clase CalculadoraHija creada y con herencia de Calculadora

---
+++ calcoohija.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+    def suma(self, op1, op2):
+        return op1 + op2
+
+    def resta(self, op1, op2):
+        return op1 - op2
+
+class CalculadoraHija(Calculadora):
+    def multiplicar(self, op1, op2):
+        return op1 * op2
+
+    def dividir(self, op1, op2):
+        try:
+            return op1 / op2
+        except ZeroDivisionError:
+            sys.exit("Error:No puedes dividir entre 0")
+
+if __name__ == '__main__':
+    calculadora=CalculadoraHija()
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: No es un parametro numerico")
+
+    if sys.argv[2] == "suma":
+        result = calculadora.suma(operando1, operando2)
+
+    elif sys.argv[2] == "resta":
+        result = calculadora.resta(operando1, operando2)
+
+    elif sys.argv[2] == "multiplica":
+        result = calculadora.multiplicar(operando1, operando2)
+
+    elif sys.argv[2] == "divide":
+        result = calculadora.dividir(operando1, operando2)
+
+    else:
+        sys.exit('Operación sólo puede ser suma o resta')
+
+    print(result)
