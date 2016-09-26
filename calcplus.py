92163cc021a9a2f2c6c4733ef59117a67261aa20
Patricia Duran Castro <pcastro@gamma06.aulas.gsyc.urjc.es>
2016-09-26 15:49:38 +0200

    calcplus vers corregida

--- calcplus.py
+++ calcplus.py
@@ -5,43 +5,34 @@ import sys
 import calcoohija
 
 if __name__ == '__main__':
-    fichero=open('fichero.txt','r')
-    lineas=fichero.readlines()
+    fichero = open('fichero.txt', 'r')
+    lineas = fichero.readlines()
     calculadora = calcoohija.CalculadoraHija()
-    
-    #print(lineas)
+
     for linea in lineas:
-        lista= linea[:-1].split(',')
+        lista = linea[:-1].split(',')
         nuevalista = lista[1:]
-        nuevalista = [int(i) for i in nuevalista]   #Convertir el string a int
-        
+        nuevalista = [int(i) for i in nuevalista]
         primernum = nuevalista[0]
         restonum = nuevalista[1:]
-        
-        
         if lista[0] == "suma":
-            resultado=primernum
+            resultado = primernum
             for nuevalista in restonum:
-                resultado = calculadora.suma(resultado,nuevalista)
+                resultado = calculadora.suma(resultado, nuevalista)
             print(resultado)
-            
-        
         if lista[0] == "resta":
-            resultado=primernum
+            resultado = primernum
             for nuevalista in restonum:
-                resultado = calculadora.resta(resultado,nuevalista)
-            print(resultado)        
-            
-        if lista[0] == "multiplica":
-            resultado=primernum
-            for nuevalista in restonum:
-                resultado = calculadora.multiplicar(resultado,nuevalista)
+                resultado = calculadora.resta(resultado, nuevalista)
             print(resultado)
-            
-        if lista[0] == "divide":
-            resultado=primernum
+        if lista[0] == "multiplica":
+            resultado = primernum
             for nuevalista in restonum:
-                resultado =calculadora.dividir(resultado,nuevalista)
-            print(resultado)        
- 
+                resultado = calculadora.multiplicar(resultado, nuevalista)
+            print(resultado)
+        if lista[0] == "divide":
+            resultado = primernum
+            for nuevalista in restonum:
+                resultado = calculadora.dividir(resultado, nuevalista)
+            print(resultado)
     fichero.close()
