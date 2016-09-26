169854bcb4176a55d3726bed4d0c07ce711382c1
Patricia Duran Castro <pcastro@gamma06.aulas.gsyc.urjc.es>
2016-09-26 16:29:32 +0200

    Corregidos errores de calc

--- calc.py
+++ calc.py
@@ -6,21 +6,21 @@ import sys
 
 def plus(op1, op2):
     """ Function to sum the operands """
-    return op1 + op2
+    return op1+op2
 
 
 def minus(op1, op2):
     """ Function to substract the operands """
-    return op1 - op2
+    return op1-op2
 
 
 def mult(op1, op2):
-	return op1 * op2
+    return op1*op2
 
 
 def div(op1, op2):
     try:
-        return op1/ op2
+        return op1/op2
     except ZeroDivisionError:
         sys.exit("Error:No puedes dividir un numero 0")
 
@@ -31,7 +31,6 @@ if __name__ == "__main__":
         operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: No es un parametro numerico")
-
 
     if sys.argv[2] == "suma":
         result = plus(operando1, operando2)
@@ -46,6 +45,6 @@ if __name__ == "__main__":
         result = div(operando1, operando2)
 
     else:
-        sys.exit('Operación sólo puede ser suma, resta, multiplicar o dividir.')
+        sys.exit('Operación no valida.')
 
     print(result)
